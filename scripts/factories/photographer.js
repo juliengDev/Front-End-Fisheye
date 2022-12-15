fa9e4f20933a179eda45558a52cbfa0ca7232e57
Julien <juliengilbert.dev@gmail.com>
2022-12-15 11:14:32 +0100

    Parametrage de la redirection vers la page Photographer

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,15 +1,20 @@
 function photographerFactory(data) {
 
-    const { city, name, price, tagline, portrait } = data; 
+    const { city,id, name, price, tagline, portrait } = data;
+    console.log(data)
     const picture = `assets/photographers/${portrait}`; 
-   
-      
-
+          
+    const url = new URL('http://127.0.0.1:5501/photographer.html');
+    url.searchParams.set('q', data.id);
+    const href = url.href;
+        
+     
     function getUserCardDOM() {
         
         // Declarer les elements du DOM
         const article = document.createElement( 'article' ); 
         const img = document.createElement( 'img' );
+        const a = document.createElement('a');
         const h2 = document.createElement( 'h2' ); 
         const cityElement = document.createElement( 'div' );
         const taglineElement = document.createElement( 'div' );
@@ -17,20 +22,22 @@ function photographerFactory(data) {
 
         
         // Modifier les elements du DOM
+        img.className = "img"
         img.setAttribute("src", picture);
-        img.style.borderRadius = "50%";
-        h2.textContent = name;       
+        a.setAttribute("href", href)        
+        h2.textContent = name;              
         cityElement.className = "city";
         cityElement.textContent = city;
         taglineElement.className = "tagline";
         taglineElement.textContent = tagline;
         priceElement.className = "price";
-        priceElement.textContent = `${price}€/jour` ;
+        priceElement.textContent = `${price}€/jour`;
         
 
         // Ajouter les elements au DOM
         article.appendChild(img);
-        article.appendChild(h2); 
+        article.appendChild(a);
+        a.appendChild(h2);        
         article.appendChild(cityElement);
         article.appendChild(taglineElement);
         article.appendChild(priceElement);
@@ -38,8 +45,10 @@ function photographerFactory(data) {
         return (article); // Retourne le contenu de la variable article a l'appel de la fonction - Pourquoi entre parenthese ?? syntaxe ??
 
     }
-    return { city, name, picture, price, tagline, getUserCardDOM } // Ne comprends pas la syntaxe : utilisation des {} ?
+    
+    return { city,id, name, picture, price, tagline, getUserCardDOM } // Ne comprends pas la syntaxe : utilisation des {} ?
 }
+
 
 
 // La balise article est une zone focusable contentant le h2 et l'image
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,24 +1,31 @@
+// Fonction qui recupere le fichier JSON 
+// et le retourne sous la forme d'un tableau
+
 async function getPhotographers() {
 
-    // Penser à remplacer par les données récupérées dans le json
+    
     const url = '/data/photographers.json';
-
     const requete = await fetch(url, {
         method: 'GET'
     });
+
     if (!requete.ok){
         alert('Un probleme est survenu. Veuillez recharger la page ulterieurement')
     }else{
         let data = await requete.json();
-        console.log(data.photographers);
-        const photographers = data.photographers;    
+        // console.log(data);
+        const photographers = data.photographers;
     
-    // et bien retourner le tableau photographers seulement une fois
+    
     return {photographers: [...photographers]} // REST parameter
     }
 }   
 
 
+// Fonction qui selectionne la div class.photographer_section
+// Puis via une boucle :
+// - va stocker chaque objet associer a un photographe (1)
+// - va ajouter pour chacun de ces objets les elements choisies au DOM (2)
 
 async function displayData(photographers) {
     const photographersSection = document.querySelector(".photographer_section");
@@ -26,13 +33,13 @@ async function displayData(photographers) {
     photographers.forEach((photographer) => {
         const photographerModel = photographerFactory(photographer);
         const userCardDOM = photographerModel.getUserCardDOM();
-        photographersSection.appendChild(userCardDOM);
+        photographersSection.appendChild(userCardDOM); //2
     });
 };
 
 async function init() {
     // Récupère les datas des photographes
-    const { photographers } = await getPhotographers();
+    const { photographers } = await getPhotographers();    
     displayData(photographers);
 };
 
